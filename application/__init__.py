dab8ff64c9c60aca27869df066ba65d1c6a5b10e
Monesh Venkul <mvvenkul@gmail.com>
2024-04-04 18:32:50 -0400

    first commit

---
+++ application/__init__.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+def create_app():
+  app = Flask(__name__)
+  app.secret_key = "MoneshVenkul"
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Cognixia EMS.db'
+
+  db.init_app(app)
+
+  from .views import views
+  from .auth import auth
+  from .api import api
+
+  app.register_blueprint(views, url_prefix='/')
+  app.register_blueprint(auth, url_prefix='/')
+  app.register_blueprint(api, url_prefix='/')
+
+  from .models import Admin, Employee, Position
+
+  create_database(app)
+
+  login_manager = LoginManager()
+  login_manager.login_view = 'auth.login' 
+  login_manager.init_app(app)
+
+  @login_manager.user_loader
+  def load_admin(id):
+     return Admin.query.get(int(id))
+
+  return app
+
+def create_database(app):
+  if not path.exists('application/Cognixia EMS.db'):
+    with app.app_context():
+        db.create_all()
+        print("Created Datebase :)")
+    
